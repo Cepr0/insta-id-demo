29f3658e6a7d4b602565b5d8c5f337774b8adf5e
Cepr0 <cepr0@ukr.net>
2018-12-13 20:49:40 +0200

    Simplified test

--- src/test/java/io/github/cepr0/demo/IdGeneratorTest.java
+++ src/test/java/io/github/cepr0/demo/IdGeneratorTest.java
@@ -1,9 +1,9 @@
 package io.github.cepr0.demo;
 
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,9 +12,13 @@ public class IdGeneratorTest {
 	@Test
 	public void loadShardId() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		IdGenerator generator = new IdGenerator();
-		Method loadShardId = IdGenerator.class.getDeclaredMethod("loadShardId");
-		loadShardId.setAccessible(true);
-		int shardId = (int) loadShardId.invoke(generator);
+
+//		Method loadShardId = IdGenerator.class.getDeclaredMethod("loadShardId");
+//		loadShardId.setAccessible(true);
+//		int shardId = (int) loadShardId.invoke(generator);
+
+		//noinspection ConstantConditions
+		int shardId = ReflectionTestUtils.invokeMethod(generator, "loadShardId");
 		assertThat(shardId).isEqualTo(3);
 	}
 }
